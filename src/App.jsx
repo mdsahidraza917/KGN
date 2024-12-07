6a066114cf94f200db7325693fec58e2c48d8237
Md Sahid Raza <89832304+mdsahidraza917@users.noreply.github.com>
2024-12-07 23:46:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css' 
+
+function App() {
+  return (
+    <div>
+      <div className="header">
+        <img src="src\assets\cover.png" className='logo' />
+      </div>
+    </div>
+  )
+}
+
+export default App
